782b0a725c23ae7d68cd6ee538f87608fc0f2035
dmigwi <migwi.ndungu@andela.com>
2018-06-14 19:12:08 +0300

    Convert chartData tags into small lettered words

--- db/dbtypes/types.go
+++ db/dbtypes/types.go
@@ -311,12 +311,12 @@ type AddressRow struct {
 // ChartsData defines the fields that will be store the
 // x , y and y2 values needed to plot the charts on the frontend.
 type ChartsData struct {
-	Time   string  `json:"Time,omitempty"`
-	Value  uint64  `json:"Value,omitempty"`
-	Size   uint64  `json:"Size,omitempty"`
-	Count  uint64  `json:"Count,omitempty"`
-	SizeF  float64 `json:"SizeF,omitempty"`
-	ValueF float64 `json:"ValueF,omitempty"`
+	Time   string  `json:"time,omitempty"`
+	Value  uint64  `json:"value,omitempty"`
+	Size   uint64  `json:"size,omitempty"`
+	Count  uint64  `json:"count,omitempty"`
+	SizeF  float64 `json:"sizef,omitempty"`
+	ValueF float64 `json:"valuef,omitempty"`
 }
 
 // ScriptPubKeyData is part of the result of decodescript(ScriptPubKeyHex)
